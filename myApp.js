1b76fc5c6729e761ded919d68652c8229e05726b
Nazmiyeh Sadiyeh <89844601+NazmiyehSadiyeh@users.noreply.github.com>
2025-09-08 14:18:58 +0200

    used res.sendFile

--- myApp.js
+++ myApp.js
@@ -22,8 +22,10 @@ let app = express();
 
 
 
-    console.log("Hello World");
-    app.get("/", (req, res) => res.send("Hello Express"));
+    //console.log("Hello World");
+    //app.get("/", (req, res) => res.send("Hello Express"));
+
+    app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 
 
